415029462d43b43110e04d1b597b633a2f31beb1
unknown <matheusfnx@hotmail.com>
2024-02-29 13:45:19 -0300

    style: remove unnecesasry comments

--- performance_with_no_uv.py
+++ performance_with_no_uv.py
@@ -12,7 +12,3 @@ end_time = time.time()
 
 elapsed_time = end_time - start_time
 print(f"Requirements.txt loading time: {elapsed_time} seconds")
-
-# No UV: 182.17838215827942 seconds or 3 minutes
-# UV: 26.280544996261597 seconds or 0.4 minutes
-# Optimized in 86,67%
